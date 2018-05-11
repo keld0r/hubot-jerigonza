0d1f483bd002d066cc44b3813015d7187c7cbbfe
Keldor <clau.ulloar@gmail.com>
2018-05-11 18:42:23 -0300

    First commit hubot-jerigonza

---
+++ README.md
@@ -0,0 +1,35 @@
+# Hubot Jerigonza
+
+**Hubot script que cambia un mensaje a jerigonza**
+
+### Instalacion:
+
+```bash
+$ npm install hubot-jerigonza --save
+```
+
+Agregar en `external-scripts.json`:
+
+```json
+[
+  ..
+  "hubot-jerigonza"
+]
+```
+
+### Uso:
+
+```bash
+hubot> hubot en jerigonza <mensaje>
+```
+
+**Ejemplo:**
+
+```bash
+hubot> hubot en jerigonza hola que tal
+
+hubot> hopolapa qupuepe tapal
+```
+
+### Licencia:
+[MIT](https://opensource.org/licenses/MIT)
---
+++ jerigonza.js
@@ -0,0 +1,44 @@
+// Description:
+//   Hubot cambia el mensaje a jerigonza
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot jerigonza <mensaje>
+//
+// Author:
+//   @keld0r
+
+function jerigonza(phrase)
+{
+	let message = phrase.toLowerCase();
+	const vowels = ['a','e','i','o','u','\xe1','\xe9','\xed','\xf3','\xfa'];
+	const vowelsReplace = [
+		'apa',
+		'epe',
+		'ipi',
+		'opo',
+		'upu',
+		'\xe1p\xe1',
+		'\xe9p\xe9',
+		'\xedp\xed',
+		'\xf3p\xf3',
+		'\xfap\xfa'
+	];
+	for(i = 0; i < vowels.length; i++){
+    let re = new RegExp(vowels[i], "g");
+		message = message.replace(re,vowelsReplace[i]);
+	}
+  return message;
+}
+
+module.exports = function(robot) {
+  robot.respond(/en jerigonza (.*)/i, function(res) {
+    var phrase = res.match[1];
+    res.send(jerigonza(phrase));
+  });
+};
